e77dfde63634c2c22402c4bb808e1365df49fc4e
Ali Al Hamadani <a2alhama@uwaterloo.ca>
2025-12-01 22:51:00 -0500

    torch_op: clean conv1d

--- torch_op/conv1d.py
+++ torch_op/conv1d.py
@@ -2,7 +2,6 @@
 from typing import Annotated
 
 import einops
-import numpy as np
 import torch as t
 import torch.nn as nn
 import torch.nn.functional as F
@@ -22,39 +21,23 @@ class Conv1d(nn.Module):
     k = 1. / (self.in_channels * self.kernel_size)
 
     self.weight = nn.Parameter(
-      (2*t.rand(self.out_channels, self.in_channels, self.kernel_size) - 1)*np.sqrt(k)
+      (2*t.rand(self.out_channels, self.in_channels, self.kernel_size) - 1) * (k ** 0.5)
     )
 
   def forward(self, x: Annotated[Tensor, "batch in_channels length"]) -> Annotated[Tensor, "batch out_channels out_length"]:
     batch, in_channels, length = x.shape
 
-    x_padded = t.zeros(batch, in_channels, length + self.padding * 2)
-    x_padded[:, :, self.padding:self.padding+length] = x
+    x_padded = F.pad(x, (self.padding, self.padding))
     x_stride = x_padded.stride()
 
     out_length = (length + 2*self.padding - self.kernel_size)//self.stride + 1
 
-    x_s = x_padded.as_strided(size=(batch, in_channels, out_length, self.kernel_size), stride=(x_stride[0], x_stride[1], self.stride, x_stride[-1])) # should be (self.stride, stride[-1])
-    x_s = einops.repeat(x_s, "batch c_in out k -> batch c_out c_in out k", c_out=self.out_channels)
-    assert x_s.shape == (batch, self.out_channels, self.in_channels, out_length, self.kernel_size)
+    x_s = x_padded.as_strided(size=(batch, in_channels, out_length, self.kernel_size), stride=(x_stride[0], x_stride[1], self.stride * x_stride[-1], x_stride[-1]))
+    x_s = einops.rearrange(x_s, "batch c_in out k -> batch 1 c_in out k")
 
-    w_s = einops.repeat(self.weight, "c_out c_in k -> batch c_out c_in out k", batch=batch, out=out_length)
-    assert w_s.shape == (batch, self.out_channels, self.in_channels, out_length, self.kernel_size)
+    w_s = einops.rearrange(self.weight, "c_out c_in k -> 1 c_out c_in 1 k")
 
     out = x_s * w_s
-    assert out.shape == (batch, self.out_channels, self.in_channels, out_length,  self.kernel_size)
+    assert out.shape == (batch, self.out_channels, self.in_channels, out_length, self.kernel_size)
 
     return einops.reduce(out, "b c_out c_in out k -> b c_out out", "sum")
-
-if __name__ == "__main__":
-  layer = Conv1d(2, 3, 2, stride=2)
-
-  x = t.arange(0, 12, dtype=t.float32).reshape(2, 2, -1)
-  output = layer(x)
-  actual = F.conv1d(x, layer.weight, stride=2)
-
-  print(f"input: {x}")
-  print(f"output: {output}")
-  print(f"actual: {actual}")
-
-  assert t.isclose(output, actual).all()
