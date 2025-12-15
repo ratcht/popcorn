ac106e9d690f394f2cac898de315e3b30cd28d38
Ali Al Hamadani <a2alhama@uwaterloo.ca>
2025-12-15 15:41:31 -0500

    torch_op: add rmsnorm

---
+++ torch_op/rmsnorm.py
@@ -0,0 +1,43 @@
+import math
+
+import torch as t
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class RMSNorm(nn.Module):
+  def __init__(self, normalized_shape, eps=None):
+    super().__init__()
+    self.normalized_shape = normalized_shape
+    self.n = math.prod(self.normalized_shape)
+    self.dims = tuple(-i - 1 for i in range(len(self.normalized_shape)))
+    self.eps = eps
+
+    self.weight = nn.Parameter(t.ones(self.normalized_shape))
+
+  def forward(self, x: t.Tensor):
+    ms = x.square().sum(self.dims) / self.n
+    if self.eps:
+      rms = t.sqrt(self.eps + ms)
+    else:
+      rms = t.sqrt(ms)
+
+    return (x/rms) * self.weight
+
+
+if __name__ == "__main__":
+  layer = RMSNorm((3, 3))
+  x = t.arange(0, 9, dtype=t.float32).reshape(3, 3)
+
+  output = layer(x)
+  torch_output = F.rms_norm(x, (3,3), layer.weight, layer.eps)
+
+  print("INPUT =========")
+  print(x)
+  print("OUTPUT ========")
+  print(output)
+  print("===========")
+  print(torch_output)
+
+  assert t.allclose(output, torch_output)
+  print("Successful!")
